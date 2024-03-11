5ea39c5c0a99de7988bac7e9d2caedbc63b60dc2
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-03-12 01:06:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -662,24 +662,3 @@ menuItems.forEach(function (menuItem) {
 });
 
 // set your own values between here
-let maxFontSize = 28.75;
-let minFontSize = 19.625;
-let maxWidth = 41.25;
-let minWidth = 23.438;
-// set your own values between here
-
-let slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
-let yAxisIntersection = -minWidth * slope + minFontSize;
-let clampSlope = slope * 100;
-let finalClamp = `clamp(${minFontSize}rem, ${yAxisIntersection.toFixed(
-  4
-)}rem + ${clampSlope.toFixed(4)}vw, ${maxFontSize}rem);`;
-
-// copy paste the string into your css!
-console.log(finalClamp);
-
-// let preferredValue = yAxisIntersection[rem] + (slope * 100)[vw]
-// console.log(yAxisIntersection)
-// 1.25rem ,0.809rem + 1.877vw, 2.5rem
-
-// clamp(0.5rem, 0.4338009074246567rem + 0.2824434361948258vw, 0.688rem)
